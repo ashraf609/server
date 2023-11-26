a687cefa7e24d40419b94623b2249a096f81fb45
ashraf609 <ettqftqfi2222@gmail.com>
2023-11-26 14:01:46 +0100

    first

---
+++ config/server.js
@@ -0,0 +1,15 @@
+module.exports = ({ env }) => ({
+  host: env("HOST", "0.0.0.0"),
+  port: env.int("PORT", 1337),
+  app: {
+    keys: env.array("APP_KEYS", ["toBeModified1", "toBeModified2"]),
+  },
+  webhooks: {
+    populateRelations: env.bool("WEBHOOKS_POPULATE_RELATIONS", false),
+  },
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET"),
+    },
+  },
+});
